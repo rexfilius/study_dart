6cc968c6217d21fb482491452ba831069fc10797
Ifeakachukwu Osakwe <ifeakachukwuosakwe@gmail.com>
2023-01-18 10:56:47 +0100

    grpc project

--- documentation/classes/class_abstract.dart
+++
@@ -1,8 +0,0 @@
-/// Use the abstract modifier to define an abstract class - a class that can't
-/// be instantiated. Abstract classes are useful for defining interfaces, often
-/// with some implementation. If you want your abstract class to appear to be
-/// instantiable, define a factory constructor.
-/// Abstract classes often have abstract methods.
-abstract class AbstractContainer {
-  void updateChildren(); // Abstract method.
-}
--- documentation/control_flow/control_flow.dart
+++
@@ -1,8 +0,0 @@
-/// You can control the flow of your Dart code using any of the following:
-/// 1. if and else
-/// 2. for loops
-/// 3. while and do-while loops
-/// 4. break and continue
-/// 5. switch and case
-/// 6. assert
-/// You can also affect the control flow using 'try-catch' and 'throw'.
