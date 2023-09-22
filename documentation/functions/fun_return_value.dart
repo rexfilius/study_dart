56881c7118a156613a2500f3bc4d1aac0056233d
Ifeakachukwu Osakwe <ifeakachukwuosakwe@gmail.com>
2023-09-22 15:22:06 +0100

    refactor

--- documentation/functions/fun_return_value.dart
+++
@@ -1,7 +0,0 @@
-/// All functions return a value. If no return value is specified, the statement
-/// 'return null;' is implicitly appended to the function body.
-functionReturnsNothing() {}
-
-void main() {
-  assert(functionReturnsNothing() == null);
-}
