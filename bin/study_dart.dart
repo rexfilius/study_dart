1875575a912b6600320fba7dacdb59c0f7eb23f1
Ifeakachukwu Osakwe <ifeakachukwuosakwe@gmail.com>
2024-09-02 16:13:13 +0100

    refactor

--- bin/study_dart.dart
+++
@@ -1,5 +0,0 @@
-import 'package:study_dart/study_dart.dart' as study_dart;
-
-void main(List<String> arguments) {
-  print('Hello world: ${study_dart.calculate()}!');
-}
--- test/study_dart_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:study_dart/study_dart.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
