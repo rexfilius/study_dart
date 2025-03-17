0bb8c50bde57fcb585307600b085f05f63a6762d
Ifeakachukwu Osakwe <ifeakachukwuosakwe@gmail.com>
2025-03-17 10:32:05 +0100

    March 17 2025

---
+++ bin/study_dart.dart
@@ -0,0 +1,5 @@
+import 'package:study_dart/study_dart.dart' as study_dart;
+
+void main(List<String> arguments) {
+  print('Hello world: ${study_dart.calculate()}!');
+}
---
+++ test/study_dart_test.dart
@@ -0,0 +1,8 @@
+import 'package:study_dart/study_dart.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
